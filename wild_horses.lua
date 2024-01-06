56c66f539697e39bd84d76f4ac9075fbc46fcf62
totallyrin <lucy.woloszczuk@gmail.com>
2024-01-06 02:43:22 +0000

    horses no longer mission entities adds genders adds probabilities reset horse only if tamed or despawned

--- wild_horses.lua
+++ wild_horses.lua
@@ -13,6 +13,12 @@ function spawnWildHorse(modelName, coords)
         Citizen.InvokeNative(0x283978A15512B2FE, horse) -- SetRandomOutfitVariation
         -- Make horse wild
         Citizen.InvokeNative(0xAEB97D84CDF3C00B, horse, true) -- SetAnimalIsWild
+        -- Make horse not-a-mission-entity so it can be despawned naturally
+        SetEntityAsNoLongerNeeded(horse)
+        -- Set random gender (0.0 = male, 1.0 = female)
+        Citizen.InvokeNative(0x5653AB26C82938CF, horse, 41611, math.random(0, 1) * 1.0) -- SetCharExpression
+        -- Update ped variation (required after horse gender change)
+        Citizen.InvokeNative(0xCC8CA3E88256E58F, horse, false, true, true, true, false) -- UpdatePedVariation
         
         print("wild horse spawned at coordinates: x = " .. coords.x .. ", y = " .. coords.y .. ", z = " .. coords.z)
         return horse
@@ -26,13 +32,16 @@ end
 
 -- Main code
 
+-- seed random
+math.randomseed(GetGameTimer()) -- seed
+for i = 1, 5 do math.random() end -- warm up
+
 RegisterCommand('spawnWildHorse', function(source, args, rawCommand)
     spawnWildHorse(args[1], {x = tonumber(args[2]), y = tonumber(args[3]), z = tonumber(args[4])})
 end, false)
 
 Citizen.CreateThread(function()
     print("wild_horses script loaded and running")
---     spawnWildHorse("a_c_horse_mustang_wildbay", {x = -289.66, y = 841.45, z = 120.03})
     while true do
         -- get player coordinates
         local player = PlayerPedId()
@@ -45,37 +54,53 @@ Citizen.CreateThread(function()
                 if distance < spawnRadius then
                     -- check if horse already exists
                     if location.horse == nil then
-                        -- spawn horse at a random location within the spawn area
-                        local spawnCoords = {
-                            x = location.x + math.random(-spawnRadius, spawnRadius),
-                            y = location.y + math.random(-spawnRadius, spawnRadius),
-                            z = location.z
-                        }
-                        
-                        -- horse should not spawn if within 50 units of player
-                        local distanceToPlayer = #(playerCoords - vector3(spawnCoords.x, spawnCoords.y, spawnCoords.z))
-                        if distanceToPlayer < 50.0 then
-                            print("spawn loc too close to player")
-                        else
+
+                        -- generate random num between 0 and 1
+                        local p = math.random()
+                        -- check against spawnRate
+                        if p <= horseBreed.spawnRate then
+                            -- spawn horse at a random location within the spawn area
+                            local spawnCoords = {
+                                x = location.x + math.random(-spawnRadius, spawnRadius),
+                                y = location.y + math.random(-spawnRadius, spawnRadius),
+                                z = location.z + 1000.0 -- ensure horse is above ground level
+                            }
                             -- set ground level
                             num, groundZ = GetGroundZAndNormalFor_3dCoord(spawnCoords.x, spawnCoords.y, spawnCoords.z)
                             spawnCoords.z = groundZ
-                            
-                            -- select a random horse model from the breed
-                            local horseModel = horseBreed.horses[math.random(#horseBreed.horses)]
-                            -- spawn the horse
-                            location.horse = spawnWildHorse(horseModel, spawnCoords)
+
+                            -- horse should not spawn if within 50 units of player
+                            local distanceToPlayer = #(playerCoords - vector3(spawnCoords.x, spawnCoords.y, spawnCoords.z))
+                            if distanceToPlayer < 50.0 then
+                                print("spawn loc too close to player")
+                            else
+                                -- select a random horse model from the breed
+                                local horseModel = horseBreed.horses[math.random(#horseBreed.horses)]
+                                -- spawn the horse
+                                location.horse = spawnWildHorse(horseModel, spawnCoords)
+                            end
+                        else
+                            print("horse not spawned; did not meet probability")
                         end
                     else
-                        print("horse already exists")
+                        -- check if horse was despawned
+                        if not Citizen.InvokeNative(0xD42BD6EB2E0F1677, location.horse) then -- DoesEntityExist
+                            location.horse = nil
+                        else
+                            print("horse already exists")
+                        end
                     end
                 else
-                    -- if player is not in area, delete the horse
-                    -- horse despawn should be handled naturally by game?
+                    -- if area has a horse
                     if location.horse ~= nil then
---                         DeletePed(location.horse)
-                        location.horse = nil
---                         print("deleted horse")
+                        -- if horse has been tamed or despawned, reset area horse
+                        if not Citizen.InvokeNative(0x3B005FF0538ED2A9, location.horse) -- GetAnimalIsWild
+                        or not Citizen.InvokeNative(0xD42BD6EB2E0F1677, location.horse) then -- DoesEntityExist
+                            print("horse has been tamed or does not exist")
+                            location.horse = nil
+                        else
+                            print("horse still exists")
+                        end
                     end
                 end
             end
