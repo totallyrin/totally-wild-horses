aa79e1d8bd0ccbaccf09263c02a60c415c141c96
totallyrin <lucy.woloszczuk@gmail.com>
2024-01-10 22:51:33 +0000

    tweaked spawn rates

--- horse_data.lua
+++ horse_data.lua
@@ -18,14 +18,14 @@ spawnDistance = 25.0
 --- horse data stored as a list of spawn locs, and list of possible models for those areas
 --- model is picked at random when player enters one of the spawn areas
 --- each breed / group has a spawnRate, which is a percentage (1.0 = 100% chance of spawning)
---- default 0.25 for "rare" horses and 0.50 for everything else
+--- default 0.08 for "rare" horses and 0.25 for everything else
 --- commented out models are models that already get spawned by the game itself. this script DOES NOT alter existing
 --- spawn areas for horses, only adds new spawn areas
 --- where possible I have tried to keep spawn areas consistent. I.e. if there is a base game spawn for Mustangs, I
 --- have added a Mustang spawn point in the same area.
 horseData = {
     -- american paint
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 666.68, y = 1909.22, z = 219.3, horse = nil }, -- ambarino, donner falls
         { x = -1368.36, y = -2570.1, z = 74.33, horse = nil }, -- hennigan's stead
         { x = -3127.05, y = -3138.27, z = 2.62, horse = nil }, -- cholla springs, lake don julio
@@ -38,7 +38,7 @@ horseData = {
         "a_c_horse_eagleflies",
     } },
     -- american standardbred
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 469.64, y = 1513.77, z = 172.13, horse = nil }, -- cumberland forest, fort wallace
         { x = -1858.81, y = 523.15, z = 114.95, horse = nil }, -- big valley
         { x = -3533.14, y = -3333.79, z = 11.95, horse = nil }, -- cholla springs, lake don julio
@@ -51,7 +51,7 @@ horseData = {
         "a_c_horse_americanstandardbred_silvertailbuckskin",
     } },
     -- andalusian
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = -1011.04, y = -1022.52, z = 61.13, horse = nil }, -- great plains, north of blackwater
     }, horses = {
         "a_c_horse_andalusian_darkbay",
@@ -59,7 +59,7 @@ horseData = {
         "a_c_horse_andalusian_rosegray",
     } },
     -- appaloosa
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 336.46, y = 2007.97, z = 215.31, horse = nil }, -- ambarino, wapiti
         { x = -979.31, y = 814.5, z = 121.49, horse = nil }, -- cattail pond
         { x = -2494.9, y = 559.25, z = 137.26, horse = nil }, -- big valley
@@ -75,7 +75,7 @@ horseData = {
         "a_c_horse_gang_charles",
     } },
     -- arabian
-    { spawnRate = 0.25, locations = {
+    { spawnRate = 0.08, locations = {
         { x = 258.14, y = 2315.69, z = 300.85, horse = nil }, -- ambarino, wapiti
         { x = -2042.91, y = 1810.61, z = 243.52, horse = nil }, -- ambarino, lake isabella
     }, horses = {
@@ -88,7 +88,7 @@ horseData = {
         "a_c_horse_arabian_white",
     } },
     -- ardennes
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 1424.4, y = 1354.95, z = 163.25, horse = nil }, -- ambarino, o'creagh's run
         { x = -1793.89, y = -930.72, z = 107.38, horse = nil }, -- big valley, south of strawberry
     }, horses = {
@@ -98,7 +98,7 @@ horseData = {
         "a_c_horse_gang_bill",
     } },
     -- belgian draft horse
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 1231.4, y = 326.07, z = 93.15, horse = nil }, -- emerald ranch
         { x = -1555.72, y = -1598.41, z = 83.28, horse = nil }, -- great plains
     }, horses = {
@@ -106,7 +106,7 @@ horseData = {
         "a_c_horse_belgian_mealychestnut",
     } },
     -- breton
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = -2884.05, y = -252.41, z = 185.8, horse = nil }, -- big valley, owanjila
         { x = 1524.25, y = 2189.75, z = 334.68, horse = nil }, -- grizzlies east
     }, horses = {
@@ -118,7 +118,7 @@ horseData = {
         "a_c_horse_breton_steelgrey",
     } },
     -- criollo
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 1412.82, y = -2135.1, z = 50.64, horse = nil }, -- south of braithwaite manor
         { x = -3872.7, y = -3556.77, z = 53.84, horse = nil }, -- rio bravo
     }, horses = {
@@ -130,7 +130,7 @@ horseData = {
         "a_c_horse_criollo_sorrelovero",
     } },
     -- dutch warmblood
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = -18.86, y = 1339.02, z = 177.49, horse = nil }, -- cumberland forest
         { x = 1557.39, y = 2.31, z = 99.23, horse = nil }, -- scarlett meadows / emerald ranch
     }, horses = {
@@ -140,7 +140,7 @@ horseData = {
         "a_c_horse_buell_warvets",
     } },
     -- gypsy cob
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = -1211.05, y = 2173.38, z = 335.86, horse = nil }, -- ambarino, colter
         { x = -2702.84, y = -1469.91, z = 149.17, horse = nil }, -- tall tress, aurora basin
     }, horses = {
@@ -152,7 +152,7 @@ horseData = {
         "a_c_horse_gypsycob_whiteblagon",
     } },
     -- hungarian half-bred
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = -1391.68, y = -1110.36, z = 75.65, horse = nil }, -- great plains
         { x = -4117.64, y = -2582.63, z = -1.11, horse = nil }, -- cholla springs
     }, horses = {
@@ -163,7 +163,7 @@ horseData = {
         "a_c_horse_gang_john",
     } },
     -- kentucky saddler
-    --{ spawnRate = 0.5, locations = {
+    --{ spawnRate = 0.25, locations = {
     -- ADD SPAWN LOCATIONS HERE
     --}, horses = {
     --    --"a_c_horse_kentuckysaddle_black",
@@ -174,7 +174,7 @@ horseData = {
     --    "a_c_horse_gang_uncle", -- BUGGED; shows up as "Nell II". Not tested with saving tamed horse
     --} },
     -- kladruber
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 2353.74, y = 1455.02, z = 143.56, horse = nil }, -- roanoke ridge
         { x = -2901.37, y = -2155.15, z = 72.53, horse = nil }, -- hennigan's stead
     }, horses = {
@@ -186,7 +186,7 @@ horseData = {
         "a_c_horse_kladruber_white",
     } },
     -- missouri fox trotter
-    { spawnRate = 0.25, locations = {
+    { spawnRate = 0.08, locations = {
         { x = 1320.33, y = 661.62, z = 87.26, horse = nil }
     }, horses = {
         "a_c_horse_missourifoxtrotter_amberchampagne",
@@ -199,7 +199,7 @@ horseData = {
         "a_c_horse_gang_micah",
     } },
     -- morgan
-    --{ spawnRate = 0.5, locations = {
+    --{ spawnRate = 0.25, locations = {
     -- ADD SPAWN LOCATIONS HERE
     --}, horses = {
     --    "a_c_horse_morgan_bay",
@@ -209,7 +209,7 @@ horseData = {
     --    "a_c_horse_morgan_palomino",
     --} },
     -- mustang
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 564.89, y = 78.4, z = 151.89, horse = nil }, -- heartlands
         { x = -2820.49, y = -2745.67, z = 78.82, horse = nil }, -- hennigan's stead
         { x = -3486.73, y = -2081.29, z = -2.05, horse = nil }, -- cholla springs, north of armadillo
@@ -226,7 +226,7 @@ horseData = {
         "a_c_horse_gang_sadie_endlesssummer",
     } },
     -- nokota
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 510.66, y = 1114.48, z = 193.3, horse = nil }, -- cumberland forest
         { x = -782.64, y = -845.57, z = 53.2, horse = nil }, -- big valley, south of bard's crossing
         { x = -2311.65, y = -2970.0, z = 4.97, horse = nil }, -- hennigan's stead
@@ -241,7 +241,7 @@ horseData = {
         "a_c_horse_gang_charles_endlesssummer",
     } },
     -- norfolk roadster
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 2154.24, y = 122.66, z = 71.14, horse = nil }, -- roanoke ridge, kamassa river
         { x = -2598.05, y = 1088.8, z = 205.06, horse = nil }, -- big valley
     }, horses = {
@@ -253,7 +253,7 @@ horseData = {
         "a_c_horse_norfolkroadster_spottedtricolor",
     } },
     -- shire
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = -254.13, y = 1026.25, z = 127.19, horse = nil }, -- valentine
         { x = -523.57, y = -398.59, z = 82.72, horse = nil }, -- flatneck station
     }, horses = {
@@ -262,7 +262,7 @@ horseData = {
         "a_c_shire_ravenblack",
     } },
     -- suffolk punch
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 1719.27, y = -1886.4, z = 47.75, horse = nil }, -- lemoyne, bolger glade
         { x = 1009.59, y = -781.91, z = 83.63, horse = nil }, -- scarlett meadows
     }, horses = {
@@ -270,14 +270,14 @@ horseData = {
         "a_c_horse_suffolkpunch_sorrel",
     } },
     -- tennessee walker (some locs may be missing)
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 1016.22, y = 237.16, z = 116.7, horse = nil }, -- heartlands
         { x = 564.89, y = 78.4, z = 151.89, horse = nil }, -- heartlands
         { x = 301.05, y = -68.16, z = 108.97, horse = nil }, -- heartlands
         { x = 822.1, y = 2092.26, z = 279.09, horse = nil }, -- ambarino, near wapiti
         { x = -492.5, y = -385.79, z = 83.88, horse = nil }, -- flatneck station
         { x = -1391.68, y = -1110.36, z = 75.65, horse = nil }, -- great plains
-        { x = -2471.0, y = -2770.56, z = 72.16, horse = nil }, -- hennigan's stead
+        { x = -2471.0, y = -2770.256, z = 72.16, horse = nil }, -- hennigan's stead
         { x = -2820.49, y = -2745.67, z = 78.82, horse = nil }, -- hennigan's stead
         { x = -3486.73, y = -2081.29, z = -2.05, horse = nil }, -- cholla springs, north of armadillo
     }, horses = {
@@ -290,9 +290,9 @@ horseData = {
         "a_c_horse_tennesseewalker_mahoganybay",
     } },
     -- thoroughbred
-    { spawnRate = 0.5, locations = {
+    { spawnRate = 0.25, locations = {
         { x = 998.17, y = 33.41, z = 94.34, horse = nil }, -- heartlands
-        { x = -2471.0, y = -2770.56, z = 72.16, horse = nil }, -- hennigan's stead
+        { x = -2471.0, y = -2770.256, z = 72.16, horse = nil }, -- hennigan's stead
         { x = -4117.64, y = -2582.63, z = -1.11, horse = nil }, -- cholla springs
     }, horses = {
         --"a_c_horse_thoroughbred_bloodbay",
@@ -304,7 +304,7 @@ horseData = {
         "a_c_horse_gang_john_endlesssummer",
     } },
     -- turkoman
-    { spawnRate = 0.25, locations = {
+    { spawnRate = 0.08, locations = {
         { x = 2811.58, y = 2304.32, z = 157.03, horse = nil }, -- brandywine drop
         { x = -5667.54, y = -2632.1, z = -5.36, horse = nil }, -- gaptooth ridge
     }, horses = {
